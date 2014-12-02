3b411968eec4f7a01d5b10dffdf38179f0574891
Lukas Erlacher <l.erlacher@gmail.com>
2014-12-02 17:25:51 +0100

    more automation

---
+++ download.sh
@@ -0,0 +1,2 @@
+( scp -r kaunas:/srv/www/testbed/results/$(basename $(pwd))/klaipeda . || scp -r kaunas:/srv/www/testbed/results/$(basename $(pwd))/klaipeda . ) &
+( scp -r kaunas:/srv/www/testbed/results/$(basename $(pwd))/tartu . || scp -r kaunas:/srv/www/testbed/results/$(basename $(pwd))/tartu . )
--- parse.sh
+++ parse.sh
@@ -1,1 +1,1 @@
-python parsing/l2-load-latency/moongen-l2-load-latency.py $1/klaipeda/output-send-* $1/tartu/output-perf* | tee double-y-percs.dat
+python ../parsing/l2-load-latency/moongen-l2-load-latency.py klaipeda/output-send-* tartu/output-perf* | tee double-y-percs.dat
