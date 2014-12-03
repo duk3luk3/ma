9d8ecaba2f70c35c0a5c7cbefaa41c039ddaddab
Lukas Erlacher <erlacher@in.tum.de>
2014-12-03 11:16:00 +0100

    downloader fix

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
-$name = $(basename $(pwd))
+name=$(basename $(pwd))
 ( scp -r kaunas:/srv/www/testbed/results/${name}/klaipeda . || scp -r kaunas:/srv/www/testbed/results/${name}/klaipeda . ) &
 ( scp -r kaunas:/srv/www/testbed/results/${name}/tartu . || scp -r kaunas:/srv/www/testbed/results/${name}/tartu . )
 
-echo "\newcommand{\theexperiment}{${name}}" > experiment.tex
+echo "\newcommand{\theexperiment}{${name}}" | sed 's/_/\\_/g' > experiment.tex
