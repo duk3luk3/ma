ef6d66dd3b891b70ff97616de82366922572a3f2
Lukas Erlacher <l.erlacher@gmail.com>
2015-02-03 13:35:20 +0100

    add quartile ranges for latency

--- parsing/l2-load-latency/moongen-l2-load-latency.py
+++ parsing/l2-load-latency/moongen-l2-load-latency.py
@@ -97,10 +97,10 @@ irq_sel = lambda x: int(x['interrupts'].replace(',','')) / float(x['seconds']) /
 uniq = lambda xs: filter(lambda x: x is not None, [xs[i] if i == 0 or xs[i-1] != xs[i] else None for i in range(len(xs))])
 
 
-#print("#Offered Load (mpps)\tavg actual mpps sent\tRTT Lower 1.5% (us)\tRTT Lower Quartile (us)\tRTT Median (us)\tRTT Upper Quartile (us)\tRTT Upper 98.5% (us)\tAvg. CPU Load (cycles)\tCPU Load StdDev (cycles)\tInterrupts (kHz)")
+#print("#Offered Load (mpps)\tavg actual mpps sent\tRTT Lower 1% (us)\tRTT Lower Quartile (us)\tRTT Median (us)\tRTT Upper Quartile (us)\tRTT Upper 99% (us)\tAvg. CPU Load (cycles)\tCPU Load StdDev (cycles)\tInterrupts (kHz)")
 #print("x\tload_avg\trtt0\trtt1\trtt2\trtt3\trtt4\tcycles_avg\tcycles_stderr\trtt_nsamples\tnsent\tnrecvd\tfrecvd\tirq_avg\tirq_stderr")
 print("#Offered Load (mpps)\tavg actual mpps sent\tRTT Median (us)\tRTT 99th perc.\tAvg. CPU Load (cycles)\tCPU Load StdDev (cycles)\tInterrupts (kHz)")
-print("x\tload_avg\trtt2\trtt4\tcycles_avg\tcycles_stderr\trtt_nsamples\tnsent\tnrecvd\tfrecvd\tirq_avg\tirq_stderr\titr_avg\titr_stderr")
+print("x\tload_avg\trtt0\trtt1\trtt2\trtt3\trtt4\tcycles_avg\tcycles_stderr\trtt_nsamples\tnsent\tnrecvd\tfrecvd\tirq_avg\tirq_stderr\titr_avg\titr_stderr")
 
 last_load = 0
 
@@ -144,8 +144,8 @@ for run in runs:
   print("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(
     offered_load,
     sent_avg,
-#    delay_percs[0],delay_percs[1],delay_percs[2],delay_percs[3],delay_percs[4],
-    delay_percs[2],delay_percs[4],
+    delay_percs[0],delay_percs[1],delay_percs[2],delay_percs[3],delay_percs[4],
+#    delay_percs[2],delay_percs[4],
     cycle_vals[0], cycle_vals[1],
     delay_len,
     totals, totalr,
