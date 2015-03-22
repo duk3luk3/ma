d50565a59b9395630d17ec0ea53b664be9ab2267
Lukas Erlacher <l.erlacher@gmail.com>
2015-03-22 13:40:41 +0100

    foo

--- download.sh
+++ download.sh
@@ -1,8 +1,21 @@
 name=$1
+h1=$2
+h2=$3
+if [ "$h1x" == "x" ]
+then
+	h1='klaipeda'
+fi
+if [ "$h2x" == "x" ]
+then
+	h2='tartu'
+fi
+
+echo "Downloading $h1 and $h2"
+
 mkdir $name
 cd $name
-( scp -r kaunas:/srv/www/testbed/results/${name}/klaipeda . || scp -r kaunas:/srv/www/testbed/results/${name}/klaipeda . ) &
-( scp -r kaunas:/srv/www/testbed/results/${name}/tartu . || scp -r kaunas:/srv/www/testbed/results/${name}/tartu . ) &
+( scp -r kaunas:/srv/www/testbed/results/${name}/$h1 . || scp -r kaunas:/srv/www/testbed/results/${name}/$h1 . ) &
+( scp -r kaunas:/srv/www/testbed/results/${name}/$h2 . || scp -r kaunas:/srv/www/testbed/results/${name}/$h2 . ) &
 ( scp kaunas:/srv/www/testbed/results/${name}/config/comment . || scp kaunas:/srv/www/testbed/results/${name}/config/comment . ) &
 wait
 
--- parse.sh
+++ parse.sh
@@ -1,1 +1,11 @@
-python ~/git/ma/parsing/l2-load-latency/moongen-l2-load-latency.py klaipeda/output-send-* tartu/output-perf* | tee double-y-percs.dat
+h1=$1
+h2=$2
+if [ "$h1x" == "x" ]
+then
+	h1='klaipeda'
+fi
+if [ "$h2x" == "x" ]
+then
+	h2='tartu'
+fi
+python ~/git/ma/parsing/l2-load-latency/moongen-l2-load-latency.py $h1/output-send-* $h2/output-perf* | tee double-y-percs.dat
